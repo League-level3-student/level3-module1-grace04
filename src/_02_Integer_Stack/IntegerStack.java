9866210ae4f9972a0dba37c382dd1a312fb94d51
league <league@wts14-3.attlocal.net>
2020-02-12 19:57:57 -0800

    line 19

--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class IntegerStack {
 	//1. create a private array of integers
 	private int[] num = new int[0];
+	
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 
@@ -18,36 +19,42 @@ public class IntegerStack {
 		//B. set the last element of the new array equal to the integer passed into the method
 		ber[ber.length-1] = v;
 		//C. iterate through the member array and copy all the elements into the new array
-		
+		System.out.println(ber);
+		for(int n : num) {
+			ber[n] = num[n];
+		}
 		//D. set the member array equal to the new array.
-		
+		num = ber;
 	}
 	
 	//4. Complete the steps in the pop method.
 	public int pop() {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
-		
+		int last = num[num.length-1];
 		//B. create a new array that is one element smaller than the member array
-		
+		int[] bre = new int[num.length-1];
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
-		
+		for(int b : bre) {
+			bre[b] = num[b];
+		}
 		//D. set the member array equal to the new array
-		
+		num = bre;
 		//E. return the variable you created in step A
-		return 0;
+		return last;
 	}
 	
 	//5. Complete the clear method to set the
 	//   member array to a new array of length 0
 	public void clear() {
-		
+		int[] zer = new int[0];
+		num = zer;
 	}
 	
 	//6. Complete the size method to return 
 	//   the length of the member array
 	public int size() {
-		return 0;
+		return num.length;
 	}
 }
