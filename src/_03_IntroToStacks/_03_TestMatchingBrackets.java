05e693748e81cac311714ab727dcdd2c4d852967
lxwgr <lxwgr@DESKTOP-L25KQUP>
2020-04-10 11:41:01 -0700

    read random line from file

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -3,6 +3,7 @@ package _03_IntroToStacks;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 import org.junit.Test;
@@ -21,17 +22,25 @@ public class _03_TestMatchingBrackets {
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
 		Stack<String> opening = new Stack<String>();
+		Stack<String> closing = new Stack<String>();
+		ArrayList<String> stri = new ArrayList<String>();
 		for(int i=0;i<b.length();i++) {
-			if(b.contains("{")) {
+			stri.add(b.substring(i, i+1));
+			if(stri.get(i).equals("{")) {
 				opening.push("{");
+				System.out.println(opening);
 			}
-			if(b.contains("}")) {
-				opening.pop();
+			if(stri.get(i).equals("}")) {
+				closing.push("}");
+				System.out.println(closing);
 			}
 		}
-			if(opening.isEmpty()) {
-				return false;
-			}
-		return true;
+		if(b.substring(0, 1).equals("}")) {
+			return false;
+		}
+		if(opening.size()==closing.size()) {
+			return true;
+		}
+		return false;
 	}
 }
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,35 @@
 package _04_HangMan;
 
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 public class HangMan{
+	int lives;
+	int words;
+	String ans;
+	Stack<String> playWords = new Stack<String>();
 	
+	public static void main(String[] args) {
+		
+	}
 	
+	public void run() {
+		JFrame frame = new JFrame();
+		JLabel lab1 = new JLabel();
+		JLabel lab2 = new JLabel();
+		frame.add(lab1);
+		frame.add(lab2);
+		frame.setSize(200, 150);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	}
 	
+	public void doStuff() {
+		ans = JOptionPane.showInputDialog("How many words?");
+		words = Integer.parseInt(ans);
+		playWords.push(readRandomLineFromFile("dictionary.txt"))
+	}
 }
